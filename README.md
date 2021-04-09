b4995d67baf2f82c9049661752ea29b1285e4fa0
Ahhyun <0828lucy@gmail.com>
2021-04-09 16:22:11 +0900

    second commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Thingy52-RaspberryPi-Mic
+This project is based on [Nordic Thingy:52 Node.js library](https://github.com/NordicPlayground/Nordic-Thingy52-Nodejs/blob/master/README.md) example.
+
+> See [RASPBERRYPI.md](https://github.com/NordicPlayground/Nordic-Thingy52-Nodejs/blob/master/RASPBERRYPI.md) for how to set it up on a Raspberry Pi running Raspbian.
+
+## Prerequisites
+1. Download [Node.js](https://nodejs.org/en/download/) ARMv7.
+2. Extract files
+3. Copy files into local directory: `cd node-v10.15.3-linux-arm71/` `sudo cp -R * /usr/local/`
+4. Check Node.js and npm version: `node -v` `npm -v`
+5. [noble-device](https://github.com/sandeepmistry/noble-device) (A [Node.js](https://nodejs.org/en/) lib to abstract BLE (Bluetooth Low Energy) peripherals, using [noble](https://github.com/sandeepmistry/noble)).
+6. Bluetooth 4.0 USB dongle supported by [noble](https://github.com/sandeepmistry/noble) and [node-bluetooth-hci-socket](https://github.com/sandeepmistry/node-bluetooth-hci-socket#prerequisites).
+
+## Installation
+1. Install noble-device: `npm install noble-device`
+2. Install package: `npm install thingy52`
+3. Install node version manager: `sudo npm install -g n`
+4. Downgrade node to v 8.9.0 by: `sudo n 8.9.0`
+5. Install bluetooth socket: `sudo npm install bluetooth-hci-socket --unsafe-perm`
+6. Find examples `cd node_modules/thingy52/examples`
+
+## Examples
+A few examples like reading environment sensor data, reading button presses, color sensor calibration, connecting Thingy:52 to Firebase and more can be found under the examples folder.
+1. Find examples `cd node_modules/thingy52/examples`
+2. RCheck if the example you want to run has other required npm packages by opening the example. As we can see below the radio.js script requires icecast, lame and util.
+```javascript
+var Thingy = require('../index');
+var icecast = require("icecast");
+var lame = require("lame");
+var util = require('util');
+```
+3. Install required npm packages: `npm install <package name>`
+4. Run example: `sudo node <example_name>.js`
+
+## Microphone 
+1. Create a new folder and clone repository: `mkdir node_modules/thingy52/microphone`
+2. Install required npm packages: 
+```
+npm install fs
+npm install date-and-time
+```
+3. Run microphone.js: `sudo node microphone.js`
+4. When Thingy52 is connected to Raspberry Pi, press button to record
+5. Recorded file is saved as pcm file format
+6. Convert raw pcm to WAV file using ffmpeg
+> [Installing ffmpeg on Raspbian](https://blog.naver.com/chandong83/220851288433)
+
+> ffmpeg usage
+```
+ffmpeg -f s16le -ar 16000 -ac 1 -i <input file> <output file>
+```
+* -f ‘force’ format = PCM signed 16-bit little endian
+* -ar ‘audio rate’ sampling rate = 16K (voice audio)
+* -ac ‘audio channels’ = mono (1)
+* -i ‘input’ file name 
---
+++ battery_test.js
@@ -0,0 +1,175 @@
+var Thingy = require('../index');
+var fs = require('fs');
+const date = require('date-and-time');
+
+var enabled;
+var batterylog = 'batterylog.txt';
+fs.open(batterylog, 'a+', function(err,fd){
+	if (err) throw err;
+	console.log('Battery log file open complete');
+});
+var environmentlog = 'environmentlog.txt';
+fs.open(environmentlog, 'a+', function(err,fd){
+	if (err) throw err;
+	console.log('Environment log file open complete');
+});
+
+console.log('This is Thingy:52 Battery Test');
+console.log('Thingy:52 battery level service!');
+console.log('Reading Thingy environment sensors!');
+
+function onBatteryLevelChange(level) {
+	var now = date.format(new Date(), 'YYYY/MM/DD HH:mm:ss');
+	fs.appendFile('batterylog.txt', now + ' Battery level: ' + level + '%\n', function (err) {
+		if (err) throw err;
+	});
+   console.log(now + ' Battery level: ' + level + '%');
+}
+
+function onTemperatureData(temperature) {
+	fs.appendFile('environmentlog.txt', date.format(new Date(), 'YYYY/MM/DD HH:mm:ss') + ' Temperature: ' + temperature + '\n', function (err) {
+		if (err) throw err;
+	});
+}
+
+function onPressureData(pressure) {
+	fs.appendFile('environmentlog.txt', '                    ' + 'Pressure: ' + pressure + '\n', function (err) {
+		if (err) throw err;
+	});
+}
+
+function onHumidityData(humidity) {
+	fs.appendFile('environmentlog.txt', '                    ' + 'Humidity: ' + humidity + '\n', function (err) {
+		if (err) throw err;
+	});
+}
+
+function onGasData(gas) {
+	fs.appendFile('environmentlog.txt', '                    ' + 
+									  'eCO2: ' + gas.eco2 + ' TVOC: ' + gas.tvoc + '\n', function (err) {
+		if (err) throw err;
+	});
+}
+
+function onColorData(color) {
+	fs.appendFile('environmentlog.txt', '                    ' + 
+									  'Color sensor: r ' + color.red +
+                             ' g ' + color.green +
+                             ' b ' + color.blue +
+                             ' c ' + color.clear + '\n', function (err) {
+		if (err) throw err;
+	});
+}
+
+function onButtonChange(state) {
+    if (state == 'Pressed') {
+        if (enabled) {
+            enabled = false;
+            this.temperature_disable(function(error) {
+                console.log('Temperature sensor stopped! ' + ((error) ? error : ''));
+            });
+            this.pressure_disable(function(error) {
+                console.log('Pressure sensor stopped! ' + ((error) ? error : ''));
+            });
+            this.humidity_disable(function(error) {
+                console.log('Humidity sensor stopped! ' + ((error) ? error : ''));
+            });
+            this.color_disable(function(error) {
+                console.log('Color sensor stopped! ' + ((error) ? error : ''));
+            });
+            this.gas_disable(function(error) {
+                console.log('Gas sensor stopped! ' + ((error) ? error : ''));
+            });
+        }
+        else {
+            enabled = true;
+            this.temperature_enable(function(error) {
+                console.log('Temperature sensor started! ' + ((error) ? error : ''));
+            });
+            this.pressure_enable(function(error) {
+                console.log('Pressure sensor started! ' + ((error) ? error : ''));
+            });
+            this.humidity_enable(function(error) {
+                console.log('Humidity sensor started! ' + ((error) ? error : ''));
+            });
+            this.color_enable(function(error) {
+                console.log('Color sensor started! ' + ((error) ? error : ''));
+            });
+            this.gas_enable(function(error) {
+                console.log('Gas sensor started! ' + ((error) ? error : ''));
+            });
+        }
+    }
+}
+
+function onDiscover(thingy) {
+  console.log('Discovered: ' + thingy);
+
+  thingy.on('disconnect', function() {
+    console.log('Disconnected!');
+  });
+
+  thingy.connectAndSetUp(function(error) {
+    console.log('Connected! ' + error ? error : '');
+
+    thingy.on('temperatureNotif', onTemperatureData);
+    thingy.on('pressureNotif', onPressureData);
+    thingy.on('humidityNotif', onHumidityData);
+    thingy.on('gasNotif', onGasData);
+    thingy.on('colorNotif', onColorData);
+    thingy.on('buttonNotif', onButtonChange);
+    thingy.on('batteryLevelChange', onBatteryLevelChange);
+    
+    thingy.notifyBatteryLevel(function(error) {
+      console.log('Battery Level Notifications enabled! ' + ((error) ? error : ''));
+    });
+    thingy.temperature_interval_set(1000, function(error) {
+        if (error) {
+            console.log('Temperature sensor configure! ' + error);
+        }
+    });
+    thingy.pressure_interval_set(1000, function(error) {
+        if (error) {
+            console.log('Pressure sensor configure! ' + error);
+        }
+    });
+    thingy.humidity_interval_set(1000, function(error) {
+        if (error) {
+            console.log('Humidity sensor configure! ' + error);
+        }
+    });
+    thingy.color_interval_set(1000, function(error) {
+        if (error) {
+            console.log('Color sensor configure! ' + error);
+        }
+    });
+    thingy.gas_mode_set(1, function(error) {
+        if (error) {
+            console.log('Gas sensor configure! ' + error);
+        }
+    });
+
+    enabled = true;
+
+    thingy.temperature_enable(function(error) {
+        console.log('Temperature sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.pressure_enable(function(error) {
+        console.log('Pressure sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.humidity_enable(function(error) {
+        console.log('Humidity sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.color_enable(function(error) {
+        console.log('Color sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.gas_enable(function(error) {
+        console.log('Gas sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.button_enable(function(error) {
+        console.log('Button started! ' + ((error) ? error : ''));
+    });
+  });
+}
+
+Thingy.discover(onDiscover);
